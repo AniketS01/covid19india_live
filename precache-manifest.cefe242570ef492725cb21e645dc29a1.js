3773dd0ce12afd0fb24f8486f66cc3effcdb9714
Aniket <aniketshetyeajs@gmail.com>
2020-06-17 15:21:00 +0530

    Updates

---
+++ precache-manifest.cefe242570ef492725cb21e645dc29a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc198dd0b19dac188f5803758c7d13f",
+    "url": "/covid19live/index.html"
+  },
+  {
+    "revision": "0a59f7e18157696d09a9",
+    "url": "/covid19live/static/css/main.07370931.chunk.css"
+  },
+  {
+    "revision": "07fe9fd7e7d8a87e49a8",
+    "url": "/covid19live/static/js/2.3f59c383.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid19live/static/js/2.3f59c383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a59f7e18157696d09a9",
+    "url": "/covid19live/static/js/main.6dc8d911.chunk.js"
+  },
+  {
+    "revision": "7412f294f19d7543eb83",
+    "url": "/covid19live/static/js/runtime-main.1a6da9c7.js"
+  }
+]);
